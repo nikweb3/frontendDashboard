584c997eb3d461a7eb6e7684c6b60176f5892ffc
nikweb3 <nikeshsaroop15@gmail.com>
2025-02-19 19:28:39 +0200

    Average CPU usage per device

---
+++ src/components/CpuUsageBarChart.vue
@@ -0,0 +1,30 @@
+<template>
+    <Bar :data="chartData" :options="chartOptions" />
+  </template>
+  
+  <script setup lang="ts">
+  import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale
+  } from 'chart.js'
+  import { Bar } from 'vue-chartjs'
+  import { defineProps } from 'vue'
+  
+  ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+  
+  const props = defineProps({
+    chartData: {
+      type: Object,
+      required: true
+    },
+    chartOptions: {
+      type: Object,
+      required: true
+    }
+  })
+  </script>
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -6,24 +6,63 @@
       </div>
     </div>
     <div v-if="data" class="overflow-auto p-4 bg-white rounded-lg shadow-lg">
+      <CpuUsageBarChart v-if="cpuChartData" :chart-data="cpuChartData" :chart-options="chartOptions" />
       <pre>{{ data }}</pre>
     </div>
   </template>
   
   <script setup lang="ts">
-  import { ref, onMounted } from 'vue';
+  import { ref, onMounted, computed } from 'vue';
   import { fetchDevices } from '../services/deviceService';
   import { Device } from '../types';
+  import CpuUsageBarChart from '../components/CpuUsageBarChart.vue';
   
   const data = ref<Device[] | null>(null);
+  const cpuChartData = ref(null);
   
   onMounted(async () => {
     try {
       data.value = await fetchDevices();
+      cpuChartData.value = computeCpuChartData(data.value);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   });
+  
+  const computeCpuChartData = (devices: Device[]) => {
+    const labels = devices.map(device => device.name);
+    const cpuUsage = devices.map(device => device.telemetryData.reduce((acc, curr) => acc + curr.cpuUsage, 0) / device.telemetryData.length);
+  
+    return {
+      labels,
+      datasets: [
+        {
+          label: 'CPU Usage',
+          backgroundColor: '#42A5F5',
+          data: cpuUsage,
+        },
+      ],
+    };
+  };
+  
+  const chartOptions = {
+    responsive: true,
+    maintainAspectRatio: true, // Ensure aspect ratio is maintained
+    scales: {
+      y: {
+        beginAtZero: true, // Ensure y-axis starts at zero
+      },
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: 'Average CPU usage per device',
+        font: {
+          size: 18
+        }
+      }
+    }
+  };
   </script>
   
   <style scoped>
