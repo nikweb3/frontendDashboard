6b723826a43d3c81b5fa15a165998596c3a6d538
nikweb3 <nikeshsaroop15@gmail.com>
2025-02-21 10:59:52 +0200

    update page title dynamically

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,16 +6,20 @@ import Compliance from '../view/Compliance.vue'
 import ReportBuilder from '../view/ReportBuilder.vue'
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/experience', component: Experience },
-  { path: '/sustainability', component: Sustainability },
-  { path: '/compliance', component: Compliance },
-  { path: '/reportbuilder', component: ReportBuilder }
+  { path: '/', component: Home, meta: { title: 'Home' } },
+  { path: '/experience', component: Experience, meta: { title: 'Experience' } },
+  { path: '/sustainability', component: Sustainability, meta: { title: 'Sustainability' } },
+  { path: '/compliance', component: Compliance, meta: { title: 'Compliance' } },
+  { path: '/reportbuilder', component: ReportBuilder, meta: { title: 'Report Builder' } }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.afterEach((to) => {
+  document.title = `${to.meta.title} - ViadexOne`
 })
 
 export default router
